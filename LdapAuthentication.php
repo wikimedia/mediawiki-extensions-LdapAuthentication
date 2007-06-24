cd4835670b8e7b7847bc1a373c5f2e48b504a75e
Ryan Lane <laner@users.mediawiki.org>
2007-06-24 21:05:29 +0000

    Changing the info about this revision of the plugin being alpha. This revision can be considered the 1.1f release.

--- LdapAuthentication.php
+++ LdapAuthentication.php
@@ -1604,7 +1604,7 @@ class LdapAuthenticationPlugin extends AuthPlugin {
  */
 $wgExtensionCredits['other'][] = array(
 	'name' => 'LDAP Authentication Plugin',
-	'version' => '1.1f (alpha)',
+	'version' => '1.1f',
 	'author' => 'Ryan Lane',
 	'description' => 'LDAP Authentication plugin with support for multiple LDAP authentication methods',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:LDAP_Authentication',
