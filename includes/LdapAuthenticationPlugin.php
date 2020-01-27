0abe0b8a9bb1bbefeebc4fa1c31ad58ce5ee7fc2
DannyS712 <DannyS712.enwiki@gmail.com>
2020-01-27 02:00:27 +0000

    Remove use of global $wgUser
    Since there isn't a context available, and it
    isn't straightforward to provide one, fall back to
    RequestContext::getMain - its not ideal, but it allows
    moving forward with the deprecation and removal of
    $wgUser in core.
    
    Bug: T243733
    Change-Id: If4337cd038693d19921869d6dbcc65061499c6d3

--- includes/LdapAuthenticationPlugin.php
+++ includes/LdapAuthenticationPlugin.php
@@ -1149,8 +1149,6 @@ class LdapAuthenticationPlugin {
 	 * @return string
 	 */
 	public function getDomain() {
-		global $wgUser;
-
 		$this->printDebug( "Entering getDomain", NONSENSITIVE );
 
 		# If there's only a single domain set, there's no reason
@@ -1169,10 +1167,10 @@ class LdapAuthenticationPlugin {
 		}
 		# If the session domain isn't set, the user may have been logged
 		# in with a token, check the user options.
-		# If $wgUser isn't defined yet, it might be due to an LDAPAutoAuthDomain config.
-		if ( isset( $wgUser ) && $wgUser->isLoggedIn() && $wgUser->getToken( false ) ) {
+		$user = RequestContext::getMain()->getUser();
+		if ( $user->isLoggedIn() && $user->getToken( false ) ) {
 			$this->printDebug( "Pulling domain from user options.", NONSENSITIVE );
-			$domain = self::loadDomain( $wgUser );
+			$domain = self::loadDomain( $user );
 			if ( $domain ) {
 				return $domain;
 			}
