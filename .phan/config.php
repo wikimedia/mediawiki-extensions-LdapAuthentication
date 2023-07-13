e384e736aac5cec4a1b8b28635f4a286462f9895
Taavi Väänänen <hi@taavi.wtf>
2023-07-14 00:17:01 +0300

    Delete LdapAutoAuthentication
    This is dead code, unless I'm missing something painfully obvious.
    
    Change-Id: Ic80c201501840b947119b252905b557bc50bbdf2

--- .phan/config.php
+++ .phan/config.php
@@ -3,7 +3,6 @@
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['file_list'][] = 'includes/LdapAuthenticationPlugin.php';
-$cfg['file_list'][] = 'includes/LdapAutoAuthentication.php';
 $cfg['file_list'][] = 'includes/LdapPrimaryAuthenticationProvider.php';
 
 return $cfg;
