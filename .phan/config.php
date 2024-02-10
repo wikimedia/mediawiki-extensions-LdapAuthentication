89265021891ec98e07228010ad3f2eecee7795d1
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-02-10 10:49:13 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-phan-config: 0.12.1 → 0.14.0
    
    npm:
    * grunt-banana-checker: 0.11.0 → 0.11.1
    
    Change-Id: I0d0358832034d0ffc96e1a08f924f3b2f2449cd3

--- .phan/config.php
+++ .phan/config.php
@@ -2,6 +2,9 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+// TODO Create a ldap phan stub and remove this line (to bump to 8.1)
+$cfg['target_php_version'] = '7.4';
+
 $cfg['file_list'][] = 'includes/LdapAuthenticationPlugin.php';
 $cfg['file_list'][] = 'includes/LdapPrimaryAuthenticationProvider.php';
 
